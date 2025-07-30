c1803e0803528f04cea5df45739c63fbc7efbca0
MD NOMAN ISLAM <113965562+nomanislam222@users.noreply.github.com>
2025-07-30 11:07:22 +0600

    Create magic9.py

---
+++ Magic/magic9.py
@@ -0,0 +1,17 @@
+print("✨✨✨ Welcome to the Magic Trick! ✨✨✨")
+print("-------------------------------------")
+print("Think of a number between 1 and 10. 🤔")
+
+input("\n👉 Press Enter when you have your secret number...")
+
+input("\n🔢 Multiply your number by 16.\nPress Enter when done...")
+
+input("\n➕ Add 144 to the result.\nPress Enter when done...")
+
+input("\n ➗ Divide  the new result by 16.\nPress Enter when done...")
+
+input("\n➖ Now, subtract the number you choosed at starting with your answer.\nPress Enter for the big reveal...")
+
+print("\n✨✨✨ The Magic Result ✨✨✨")
+print("Your unit digit is... 🎉 9! 🎉")
+print("Isn't that magical? 🧙‍♂️✨")
